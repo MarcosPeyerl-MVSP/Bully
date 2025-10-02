c3705ae641ae1a6a2b4ac25ae757ed4977eb993a
Marcos Peyerl <marcos.peyerl@edu.fecap.br>
2025-10-01 22:29:41 -0300

    Formulario

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def index():
     """Página inicial"""
     return render_template('estatico/index.html')
 
+@app.route('/formulario')
+def formulario():
+    """Formulário"""
+    return render_template('variavel/formulario/formulario.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
